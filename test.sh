44bf09d09bf2d131608bd577795af0c174807bc2
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2024-05-27 11:23:24 +0900

    updated

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/wuzaiqiang/anaconda3/envs/openvoice/lib/python3.9/site-packages/nvidia/cudnn/lib/ python test.py
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/wuzaiqiang/anaconda3/envs/openvoice/lib/python3.9/site-packages/nvidia/cudnn/lib/
+python test.py
